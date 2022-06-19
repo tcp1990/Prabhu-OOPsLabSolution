7d7acf6993d0d7fbda51f63f24efa3e824747291
prabhu <prabhu_tc@yahoo.com>
2022-06-19 13:05:26 +0530

    [Running eclipse cleanup command]

--- src/com/lab/assessment/CredentialService.java
+++ src/com/lab/assessment/CredentialService.java
@@ -1,25 +1,26 @@
 package com.lab.assessment;
 
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class CredentialService extends Employee {
-	
+
 	public CredentialService(String fName, String lName)
 	{
 		super(fName, lName);
 	}
 
 	public void setDepartment() {
-		
+
 		System.out.print("Please enter the department from the following\n"
 				+ "1. Technical\n"
 				+ "2. Admin\n"
 				+ "3. Human Resource\n"
 				+ "4. Legal\n");
-		
+
 		Scanner in = new Scanner(System.in);
 		int depChoice = in.nextInt();
-		
+
 		switch(depChoice)
 		{
 		case 1:
@@ -40,16 +41,16 @@ public class CredentialService extends Employee {
 		}
 		in.close();
 	}
-	
+
 	public void generatePassword()
 	{
 		int len = 8;
 		String capitalChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		String smallChars = capitalChars.toLowerCase();
 		String numbers = "0123456789";
-		String symbols = "!@#$%^&*_=+-/.?<>)";		
+		String symbols = "!@#$%^&*_=+-/.?<>)";
 		String values = capitalChars + smallChars + numbers + symbols;
-		
+
 		Random rndm_method = new Random();
 
 		char[] password = new char[len];
@@ -61,13 +62,13 @@ public class CredentialService extends Employee {
 		var strPwd = new String(password);
 		setPassword(strPwd);
 	}
-	
+
 	public void generateEmail()
 	{
 		String eMail = getFirstName().toLowerCase() + getLastName().toLowerCase() + "@" + getDepartment() + ".company.com";
 		setEmail(eMail);
 	}
-	
+
 	public void showCredentials()
 	{
 		System.out.println("Dear "+ getFirstName() +" your generated credentials are as follows\r\n"
--- src/com/lab/assessment/Employee.java
+++ src/com/lab/assessment/Employee.java
@@ -6,48 +6,48 @@ public class Employee {
 	private String eMail;
 	private String pWord;
 	private String department;
-	
+
 	public Employee(String fName,String lName)
 	{
 		firstName = fName;
 		lastName = lName;
 	}
-	
+
 	public String getFirstName()
 	{
 		return firstName;
 	}
-	
+
 	public String getLastName()
 	{
 		return lastName;
 	}
-	
+
 	public String getEmail()
 	{
 		return eMail;
 	}
-	
+
 	public void setEmail(String email)
 	{
 		eMail = email;
 	}
-	
+
 	public String getPassword()
 	{
 		return pWord;
 	}
-	
+
 	public void setPassword(String password)
 	{
 		pWord = password;
 	}
-	
+
 	public String getDepartment()
 	{
 		return department;
 	}
-	
+
 	public void setDepartment(String depMent)
 	{
 		department = depMent;
